88caf95f5cca78c28d43097d381e24049ba1e685
Tajeu2001 <tajeusanta@gmail.com>
2021-07-18 14:39:59 +0300

    Create forms

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile,Project,Rating
+
 
 
 class SignUpForm(UserCreationForm):
@@ -9,4 +11,28 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic','bio','contact']
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email','first_name','last_name',]
+
+class PostProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user','design_rating','content_rating','usability_rating']
+        widgets = {
+            'project_categories': forms.CheckboxSelectMultiple()
+        }
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        exclude = ['overall_score','project','user']
 
