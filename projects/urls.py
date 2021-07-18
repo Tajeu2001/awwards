e6f9c725fdd4f91d419727199da52e0f46a5c177
Tajeu2001 <tajeusanta@gmail.com>
2021-07-18 14:36:03 +0300

    Add url to the profile page

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('account/', include('django.contrib.auth.urls')),
   path('',views.index,name='index'),
+  path('profile',views.profile,name='profile'),
+  path('update_profile',views.update_profile,name='update_profile'),
+  path('post_project',views.post_project,name='newProject'),
+  path('project',views.project,name='project'),
 ]
