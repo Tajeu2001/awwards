5951258d0f581fd96295752a5b362e88c441373f
Tajeu2001 <tajeusanta@gmail.com>
2021-07-20 14:11:55 +0300

    Include urls to the rating page

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
 
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('account/', include('django.contrib.auth.urls')),
   path('',views.index,name='index'),
-  path('profile',views.profile,name='profile'),
+  path('profile/<username>',views.profile,name='profile'),
   path('update_profile',views.update_profile,name='update_profile'),
   path('post_project',views.post_project,name='newProject'),
-  path('project',views.project,name='project'),
+  path('project/<project_id>',views.project,name='project'),
+  path('rate/<project_id>',views.rate,name='rate'),
+  path('api/profile/',views.ProfileList.as_view()),
+  path('api/project/',views.ProjectList.as_view()),
+  path('api/profile/profile_id/',views.ProfileDescription.as_view()),
+  path('api/project/project_id/<pk>/',views.ProjectDescription.as_view()), 
 ]
